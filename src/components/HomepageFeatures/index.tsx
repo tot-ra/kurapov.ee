a1d79a9043222905368b0525eb244fe73497b225
Artjom Kurapov <artkurapov@gmail.com>
2024-11-27 04:11:28 +0200

    ююю

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -8,12 +8,6 @@ export default function HomepageFeatures(): JSX.Element {
           <iframe width="600" height="340" src="https://www.youtube.com/embed/aLRu5WpA1dU" 
           title="О канале" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-
-
-          <iframe 
-          src="https://open.spotify.com/embed/album/2lulBSRsNIxUwTQQdlwjwK?utm_source=generator&theme=0" 
-          width="100%" height="500" 
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           
         </div>
       </div>
